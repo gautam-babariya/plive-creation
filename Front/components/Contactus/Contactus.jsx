75f8c317325db7a0fc87cd6a086803d36b08f2b0
gautam <gautambabariya0125@gmail.com>
2024-12-26 21:16:18 +0530

    krt

--- Front/components/Contactus/Contactus.jsx
+++ Front/components/Contactus/Contactus.jsx
@@ -35,7 +35,7 @@ function Contactus() {
         };
         const contacturl = 'plivecreation-api.vercel.app/contact';
         try {
-            const response = await axios.post(contacturl, productData, {
+            const response = await axios.post('plivecreation-api.vercel.app/contact', productData, {
                 headers: {
                     'Content-Type': 'application/json',
                 }
