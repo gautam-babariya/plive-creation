509fb5f11354c04891edc53080d5c711e44c62d8
gautam <gautambabariya0125@gmail.com>
2024-12-29 17:54:48 +0530

    hyt

--- Front/components/Pricing/Pricing.jsx
+++ Front/components/Pricing/Pricing.jsx
@@ -17,22 +17,21 @@ function Pricing() {
                         <div class="pricing_card card">
                             <div class="card-body pricing_cardbody">
                                 <h5 class="card-title pricing_cardtitle">Base plan<br></br>
-                                    <p className='pricing_money'>₹ 2999</p></h5>
+                                    <p className='pricing_money'>₹ 1999</p></h5>
                             </div>
                             <ul class="list-group list-group-flush">
                                 <li class="list-group-item pricing_items">1 pages in website<img src={check} alt="check" className="pricing_checkimg" /></li>
                                 <li class="list-group-item pricing_items">1 Year Free Domain Name<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">1 Year Free Hosting<img src={check} alt="check" className="pricing_checkimg" /></li>
+                                <li class="list-group-item pricing_items">Free Hosting on vercel<img src={check} alt="check" className="pricing_checkimg" /></li>
                                 <li class="list-group-item pricing_items">Free SSL Certificates<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">Unlimited Email Accounts<img src={check} alt="check" className="pricing_checkimg" /></li>
                                 <li class="list-group-item pricing_items">SEO Friendly Website<img src={check} alt="check" className="pricing_checkimg" /></li>
                                 <li class="list-group-item pricing_items">Responsive Website<img src={check} alt="check" className="pricing_checkimg" /></li>
                                 <li class="list-group-item pricing_items">Dynamic Website<img src={check} alt="check" className="pricing_checkimg" /></li>
                                 <li class="list-group-item pricing_items">Social Media Integration<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">Unlimited Bandwidth<img src={check} alt="check" className="pricing_checkimg" /></li>
+                                <li class="list-group-item pricing_items">100GB per month Bandwidth<img src={check} alt="check" className="pricing_checkimg" /></li>
                                 <li class="list-group-item pricing_items">24/7 Technical Support<img src={check} alt="check" className="pricing_checkimg" /></li>
                                 <li class="list-group-item pricing_items">Free 3 months maintenance<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">Delivered within 3 days<img src={check} alt="check" className="pricing_checkimg" /></li>
+                                <li class="list-group-item pricing_items">Delivered within 3-7 days<img src={check} alt="check" className="pricing_checkimg" /></li>
                             </ul>
                             <div className='pricing_buttondiv'>
                                 <button onClick={() => (window.location.href = 'tel:9157528788')} className="pricing_button">Call now</button>
@@ -59,7 +58,7 @@ function Pricing() {
                                 <li class="list-group-item pricing_items">Unlimited Bandwidth<img src={check} alt="check" className="pricing_checkimg" /></li>
                                 <li class="list-group-item pricing_items">24/7 Technical Support<img src={check} alt="check" className="pricing_checkimg" /></li>
                                 <li class="list-group-item pricing_items">Free 6 months maintenance<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">Delivered within 3 days<img src={check} alt="check" className="pricing_checkimg" /></li>
+                                <li class="list-group-item pricing_items">Delivered within 3-7 days<img src={check} alt="check" className="pricing_checkimg" /></li>
                             </ul>
                             <div className='pricing_buttondiv'>
                                 <button onClick={() => (window.location.href = 'tel:9157528788')} className="pricing_button">Call now</button>
