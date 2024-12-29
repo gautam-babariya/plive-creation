64c376108610de42908e71ed32ddb48e2c30782c
gautam <gautambabariya0125@gmail.com>
2024-12-29 16:11:55 +0530

    allsetwh

--- Front/pages/Landingpage/Landingpage.jsx
+++ Front/pages/Landingpage/Landingpage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Landingpage.css'
 import Hero from '../../components/Heropage/Hero'
 import Aboutus from '../../components/Aboutus/Aboutus'
 import Services from '../../components/Services/Services'
@@ -8,8 +9,12 @@ import Contactus from '../../components/Contactus/Contactus'
 import Footer from '../../components/Footer/Footer'
 
 function Landingpage() {
+  const phoneNumber = "919157528788"; // Replace with your number
+  const message = "Hello, can I get more information about your services?"; 
+  const encodedMessage = encodeURIComponent(message); // Encode the message for the URL
   return (
     <>
+      <div>
         <Hero />
         <Aboutus />
         <Services />
@@ -17,6 +22,10 @@ function Landingpage() {
         <Samples />
         <Contactus />
         <Footer />
+        <a href={`https://wa.me/${phoneNumber}?text=${encodedMessage}`} target="_blank" class="landing_whatsapp-icon">
+          <img id='landing_whatsappiconimg' src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp" />
+        </a>
+      </div>
     </>
   )
 }
