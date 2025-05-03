1d453b0731840846f57dcaf58504204103fdb61d
gautam <gautambabariya0125@gmail.com>
2025-05-03 10:09:40 +0530

    pricing

--- Front/components/Pricing/Pricing.jsx
+++ Front/components/Pricing/Pricing.jsx
@@ -14,48 +14,76 @@ function Pricing() {
                 <div className="pricing_carddiv">
                     {/* base  */}
                     <div className="pricing_container">
-                        <div class="pricing_card card">
-                            <div class="card-body pricing_cardbody">
-                                <h5 class="card-title pricing_cardtitle">business plan<br></br>
-                                    <p className='pricing_money'>₹1999</p></h5>
+                        <div className="pricing_card card">
+                            <div className="card-body pricing_cardbody">
+                                <h5 className="card-title pricing_cardtitle">
+                                    Business Plan<br />
+                                    <p className='pricing_money'>₹6,999</p>
+                                </h5>
                             </div>
-                            <ul class="list-group list-group-flush">
-                                <li class="list-group-item pricing_items">5 pages in website<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">Free SSL Certificates<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">Social Media Integration<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">SEO Friendly Website<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">Responsive Design<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">Static Website<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">24/7 Technical Support<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">Free 3 months maintenance<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">Delivered within 1 week<img src={check} alt="check" className="pricing_checkimg" /></li>
+                            <ul className="list-group list-group-flush">
+                                <li className="list-group-item pricing_items">
+                                    5 Pages in Website <img src={check} alt="check" className="pricing_checkimg" />
+                                </li>
+                                <li className="list-group-item pricing_items">
+                                    1-year free hosting <img src={check} alt="check" className="pricing_checkimg" />
+                                </li>
+                                <li className="list-group-item pricing_items">
+                                    Custom Design <img src={check} alt="check" className="pricing_checkimg" />
+                                </li>
+                                <li className="list-group-item pricing_items">
+                                    Contact Form with WhatsApp Chat<img src={check} alt="check" className="pricing_checkimg" />
+                                </li>
+                                <li className="list-group-item pricing_items">
+                                    Basic SEO <img src={check} alt="check" className="pricing_checkimg" />
+                                </li>
+                                <li className="list-group-item pricing_items">
+                                    Mobile & Tablet Responsive Design <img src={check} alt="check" className="pricing_checkimg" />
+                                </li>
+                                <li className="list-group-item pricing_items">
+                                    Free SSL Certificates <img src={check} alt="check" className="pricing_checkimg" />
+                                </li>
+                                <li className="list-group-item pricing_items">
+                                    24/7 Technical Support <img src={check} alt="check" className="pricing_checkimg" />
+                                </li>
+                                <li className="list-group-item pricing_items">
+                                    Free 1 Month Maintenance <img src={check} alt="check" className="pricing_checkimg" />
+                                </li>
+                                <li className="list-group-item pricing_items">
+                                    Delivered within 7 days <img src={check} alt="check" className="pricing_checkimg" />
+                                </li>
                             </ul>
                             <div className='pricing_buttondiv'>
-                                <button onClick={() => (window.location.href = 'tel:9157528788')} className="pricing_button">Call now</button>
+                                <button onClick={() => (window.location.href = 'tel:9157528788')} className="pricing_button">
+                                    Call Now
+                                </button>
                             </div>
                         </div>
                     </div>
+
                     {/* standard  */}
                     <div className="pricing_container">
                         <div class="pricing_card card">
                             <div class="card-body pricing_cardbody">
                                 <h5 class="card-title pricing_cardtitle">Basic Ecommerce<br></br><p id='pricing_popular'>(most popular)</p>
-                                    <p className='pricing_money'>₹4999</p></h5>
+                                    <p className='pricing_money'>₹14,999</p></h5>
                             </div>
                             <ul class="list-group list-group-flush">
                                 <li class="list-group-item pricing_items">10 pages in website<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">Admin panel<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">User dashboard<img src={check} alt="check" className="pricing_checkimg" /></li>
+                                <li class="list-group-item pricing_items">1-year free hosting<img src={check} alt="check" className="pricing_checkimg" /></li>
+                                <li class="list-group-item pricing_items">Unlimited Products<img src={check} alt="check" className="pricing_checkimg" /></li>
+                                <li class="list-group-item pricing_items">Shopping Cart + Checkout System<img src={check} alt="check" className="pricing_checkimg" /></li>
                                 <li class="list-group-item pricing_items">Free SSL Certificates<img src={check} alt="check" className="pricing_checkimg" /></li>
                                 <li class="list-group-item pricing_items">Payment gateway<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">Add cart<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">SEO Friendly Website<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">Responsive Design<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">Dynamic Website<img src={check} alt="check" className="pricing_checkimg" /></li>
+                                <li class="list-group-item pricing_items">Product Search & Filter
+                                    <img src={check} alt="check" className="pricing_checkimg" /></li>
+                                <li class="list-group-item pricing_items">Basic SEO<img src={check} alt="check" className="pricing_checkimg" /></li>
+                                <li class="list-group-item pricing_items">Mobile Responsive Design<img src={check} alt="check" className="pricing_checkimg" /></li>
+                                <li class="list-group-item pricing_items">Admin panel<img src={check} alt="check" className="pricing_checkimg" /></li>
                                 <li class="list-group-item pricing_items">Social Media Integration<img src={check} alt="check" className="pricing_checkimg" /></li>
                                 <li class="list-group-item pricing_items">24/7 Technical Support<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">Free 3 months maintenance<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">Delivered within 2 weeks<img src={check} alt="check" className="pricing_checkimg" /></li>
+                                <li class="list-group-item pricing_items">Free 1 month maintenance<img src={check} alt="check" className="pricing_checkimg" /></li>
+                                <li class="list-group-item pricing_items">Delivered within 14 days<img src={check} alt="check" className="pricing_checkimg" /></li>
                             </ul>
                             <div className='pricing_buttondiv'>
                                 <button onClick={() => (window.location.href = 'tel:9157528788')} className="pricing_button">Call now</button>
@@ -67,22 +95,16 @@ function Pricing() {
                         <div class="pricing_card card">
                             <div class="card-body pricing_cardbody">
                                 <h5 class="card-title pricing_cardtitle">Advance Ecommerce<br></br>
-                                    <p className='pricing_money'>₹9999</p></h5>
+                                    <p className='pricing_money'>₹19,999</p></h5>
                             </div>
                             <ul class="list-group list-group-flush">
                                 <li class="list-group-item pricing_items">20 pages in website<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">multi Admin panel<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">User dashboard<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">Free SSL Certificates<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">multi Payment gateway<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">Add cart with extra feature<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">SEO Friendly Website<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">Responsive Design<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">Dynamic Website<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">Social Media Integration<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">24/7 Technical Support<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">Free 3 months maintenance<img src={check} alt="check" className="pricing_checkimg" /></li>
-                                <li class="list-group-item pricing_items">Delivered within 3 weeks<img src={check} alt="check" className="pricing_checkimg" /></li>
+                                <li class="list-group-item pricing_items">1-year free hosting<img src={check} alt="check" className="pricing_checkimg" /></li>
+                                <li class="list-group-item pricing_items">All features in basic ecommerce<img src={check} alt="check" className="pricing_checkimg" /></li>
+                                <li class="list-group-item pricing_items">Advanced Filters<img src={check} alt="check" className="pricing_checkimg" /></li>
+                                <li class="list-group-item pricing_items">Order Tracking<img src={check} alt="check" className="pricing_checkimg" /></li>
+                                <li class="list-group-item pricing_items">Reviews & Ratings System<img src={check} alt="check" className="pricing_checkimg" /></li>
+                                <li class="list-group-item pricing_items">Delivered within 17 days<img src={check} alt="check" className="pricing_checkimg" /></li>
                             </ul>
                             <div className='pricing_buttondiv'>
                                 <button onClick={() => (window.location.href = 'tel:9157528788')} className="pricing_button">Call now</button>
